304396aaa869d44a500a309c61bf624fc4a877c8
a102306093 <102306093@nccu.edu.tw>
2016-05-19 15:03:22 +0000

    prduct_detail and supplier

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  
+  get "home/blog" => "home#blog"
+  get "home/list" => "home#list"
+  get "home/content" => "home#content"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
